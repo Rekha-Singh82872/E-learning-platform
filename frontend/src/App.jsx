7d539a353ff4af267d805754ccb214a55b20bf32
rekhasingh <rs0943652@gmail.com>
2024-12-29 17:37:21 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home"; 
+import Header from "./Components/header/Header";
 
-const App = () => {
+function App() {
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+      <Header />
+      
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- frontend/src/Components/header/header.jsx
+++ frontend/src/Components/header/header.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { Link } from "react-router-dom";
 import "./header.css";
 
-const header = () => {
+const Header = () => {
   return (
-   <header>
-    <div className>E-Learning</div>
-    <div className="link">
-        
-    </div>
-   </header>
-  )
-}
+    <header>
+      <div className="logo">E-Learning</div> {/* Corrected className */}
+      <div className="link">
+        <Link to="/">Home</Link>
+        <Link to="/courses">Courses</Link>
+        <Link to="/about">About</Link>
+        <Link to="/account">Account</Link>
+      </div>
+    </header>
+  );
+};
 
-export default header
+export default Header;
