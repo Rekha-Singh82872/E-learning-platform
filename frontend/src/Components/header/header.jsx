b5a8fe9402ed44671b401140b1f435b3dd31c894
rekhasingh <rs0943652@gmail.com>
2024-12-29 12:42:45 +0530

    first commit

---
+++ frontend/src/Components/header/header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./header.css";
+
+const header = () => {
+  return (
+   <header>
+    <div className>E-Learning</div>
+    <div className="link">
+        
+    </div>
+   </header>
+  )
+}
+
+export default header
