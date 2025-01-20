92f04569ab01aef889dde285ca9bf1df3e55a3a3
rekhasingh <rs0943652@gmail.com>
2025-01-20 14:53:33 +0530

    firstcommit

--- frontend/src/pages/auth/Verify.jsx
+++ frontend/src/pages/auth/Verify.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import "./auth.css";
+import { Link, useNavigate } from "react-router-dom";
+
 
 const Verify = () => {
   return (
-    <div>Verify</div>
+<div className="auth-page">
+  <div className="auth-form">
+    <h2>Verify Account</h2>
+    <form>
+    <label htmlFor="otp">Otp</label>
+    <input type="number"  required />
+    <button className="common-btn">Verify</button>
+    </form>
+    <p>
+          Go to <Link to="/login">Login</Link> page
+        </p>
+
+  </div>
+</div>
   )
 }
 
--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./home.css";
 
-const Home = () => {
+const Home = () => { 
+  const navigate = useNavigate();
+
   return (
-    <div>home</div>
-  )
-}
+    <div>
+     <div className="home">
+      <div className="home-content">
+        <h1>Welcome to our E-learning Platform</h1>
+        <p>Learn, Grow, Excel,</p>
+        <button onClick={() => navigate("/courses")} className="common-btn">
+            Get Started
+          </button>
+
+
+      </div>
+
+    </div>
+    </div>
+  );
+};
 
 export default Home
