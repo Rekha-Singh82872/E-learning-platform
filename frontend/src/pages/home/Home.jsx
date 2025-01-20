43e95af11cf96a6ec926086e19b5c60f1aed7f80
rekhasingh <rs0943652@gmail.com>
2025-01-20 17:00:40 +0530

    commit

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,26 +1,27 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./home.css";
+import Testimonials from "../../Components/testimonials/testimonials";
 
-const Home = () => { 
+
+
+const Home = () => {
   const navigate = useNavigate();
-
   return (
     <div>
-     <div className="home">
-      <div className="home-content">
-        <h1>Welcome to our E-learning Platform</h1>
-        <p>Learn, Grow, Excel,</p>
-        <button onClick={() => navigate("/courses")} className="common-btn">
+      <div className="home">
+        <div className="home-content">
+          <h1>Welcome to our E-learning Platform</h1>
+          <p>Learn, Grow, Excel</p>
+          <button onClick={() => navigate("/courses")} className="common-btn">
             Get Started
           </button>
-
-
+        </div>
       </div>
-
-    </div>
+      
+      <Testimonials/>
     </div>
   );
 };
 
-export default Home
+export default Home;
